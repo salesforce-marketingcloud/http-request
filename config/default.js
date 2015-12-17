c0c3c19dd62dfc7dfe50f6333a35f1e01fc57059
dsimmons <dsimmons@exacttarget.com>
2015-12-17 15:53:54 -0600

    customerKey

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+   appId           : '29172a9c-c893-456a-bc8c-648132e35390',
+   clientId        : 'rsuy7t23gnftgl6kr7lov4ow',
+   clientSecret    : 'Rr7ic191JGyKXfjA5srWU8XX',
+   appSignature    : 'hoi2cjh1liqcvr1lvqhj5w2ru44eguauia23pcqf0ujimstmetxodra1ai24vnikeb5fcq1vtdgsxflw5dqx3ijqpt0ocwo5o3nhomgxogimkqbgbn5etfp5agvx4n5dmtk3zaww3woz3jrfehceyivlpcc2cc5z3ytvkpdz3hrbkjunn5ninekevhgpbrygn5r3w4mlzabr2r0hnvxcmv0avr0fepercruu0zqkaw55da21wtxzifyblaxn15b',
+   authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
+};
