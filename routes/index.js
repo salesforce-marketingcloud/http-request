5c30cdb397ee8872568b960093fdc15bf4b47b15
dsimmons <dsimmons@exacttarget.com>
2016-03-01 16:16:31 -0600

    index view

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,19 @@ var activity = require('./activity');
  * GET home page.
  */
 exports.index = function(req, res){
+	/*
     if( !req.session.token ) {
         res.render( 'index', {
             title: 'Unauthenticated',
             errorMessage: 'This app may only be loaded via the ExactTarget Marketing Cloud',
         });
     } else {
+    */
         res.render( 'index', {
             title: 'Hello World Custom Interaction Example',
             results: activity.logExecuteData,
         });
-    }
+    //}
 };
 
 exports.login = function( req, res ) {
